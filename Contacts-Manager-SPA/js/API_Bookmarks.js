0d5b4eb40a0f0c1464e806a9b5aa95f9d2e8f8c9
tristandm96 <tristandionnemichaud@gmail.com>
2024-09-11 17:51:43 -0300

    API_Bookmark 0.1

---
+++ Contacts-Manager-SPA/js/API_Bookmarks.js
@@ -0,0 +1,41 @@
+const API_URL = "http://localhost:5000/api/Bookmarks";
+function API_GetBookmarks() {
+    return new Promise(resolve => {
+        $.ajax({
+            url: API_URL,
+            success: bookmarks => { resolve(bookmarks); },
+            error: (xhr) => { console.log(xhr); resolve(null); }
+        });
+    });
+}
+function API_GetBookmark(BookmarkId) {
+    return new Promise(resolve => {
+        $.ajax({
+            url: API_URL + "/" + BookmarkId,
+            success: bookmark => { resolve(bookmark); },
+            error: () => { resolve(null); }
+        });
+    });
+}
+function API_SaveBookmark(bookmark, create) {
+    return new Promise(resolve => {
+        $.ajax({
+            url: API_URL + (bookmark ? "" : "/" + contact.Id),
+            type: create ? "POST" : "PUT",
+            contentType: 'application/json',
+            data: JSON.stringify(bookmark),
+            success: (/*data*/) => { resolve(true); },
+            error: (/*xhr*/) => { resolve(false /*xhr.status*/); }
+        });
+    });
+}
+function API_DeleteBookmark(id) {
+    return new Promise(resolve => {
+        $.ajax({
+            url: API_URL + "/" + id,
+            type: "DELETE",
+            success: () => { resolve(true); },
+            error: (/*xhr*/) => { resolve(false /*xhr.status*/); }
+        });
+    });
+}
